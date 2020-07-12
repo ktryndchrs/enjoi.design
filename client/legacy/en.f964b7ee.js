4ac865375a653433ff7cd112efbf83f4c625b53a
ktryndchrs <catherine.desrochers@gmail.com>
2020-07-12 16:31:36 -0600

    Updates

---
+++ client/legacy/en.f964b7ee.js
@@ -0,0 +1,1 @@
+var t={about:"About"},e="About this site",o=["This is the 'about' page. There's not much here."],i={title:t,about_this_site:"About this site",about_content:o};export default i;export{o as about_content,e as about_this_site,t as title};
---
+++ client/legacy/en.f9ca21f0.js
@@ -0,0 +1,1 @@
+var e="Enjoi.design",o="Enjoi.design",i="Hello World",d={title:"Enjoi.design",heading:"Enjoi.design",body:"Hello World"};export default d;export{i as body,o as heading,e as title};
